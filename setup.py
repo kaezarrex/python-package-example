de27328f3ea1f428afaf5a8672230cb67ef1e313
David Hazinski <dhazinski@gmail.com>
2013-10-14 14:46:01 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      long_description=readme(),
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      scripts=['bin/funniest-joke'],
+      entry_points = {
+          'console_scripts': ['funniest-joke2=funniest.command_line:main'],
+      },
+      install_requires=[
+          'markdown',
+      ],
+      test_suite='nose.collector',
+      test_requite=['nose'],
+      include_package_data=True,
+      zip_safe=False)
